51dc4e3ad4a5220387c20c32972202bf61c8e6a0
Darren Mulholland <dmulholland@outlook.ie>
2015-08-10 14:34:46 +0100

    Python: add command support to ArgSet

--- python/clio.py
+++ python/clio.py
@@ -10,7 +10,7 @@ import sys
 
 
 # Library version number.
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 
 # Internal class for storing option data.
@@ -242,7 +242,7 @@ class ArgParser:
                 callback = self.callbacks[command]
                 argset = parser.parse(argstream.remainder())
                 callback(argset)
-                break;
+                return ArgSet(self.options, self.arguments, command, argset)
 
             # Is the argument the automatic 'help' command?
             elif argstream.peek() == "help":
@@ -267,13 +267,19 @@ class ArgParser:
 # An ArgSet instance represents a set of parsed arguments.
 class ArgSet:
 
-    def __init__(self, options, arguments):
+    def __init__(self, options, arguments, command=None, argset=None):
 
         # Stores a dictionary of option objects indexed by option name.
         self.options = options
 
         # Stores a list of positional arguments.
         self.arguments = arguments
+
+        # Stores the command string, if a command was found.
+        self.command = command
+
+        # Stores the command's ArgSet instance, if a command was found.
+        self.argset = argset
 
     # Enable dictionary-style access to options: value = argset['name'].
     def __getitem__(self, name):
@@ -301,6 +307,12 @@ class ArgSet:
         else:
             lines.append("  [none]")
 
+        lines.append("\nCommand:")
+        if self.has_cmd():
+            lines.append("  %s" % self.get_cmd())
+        else:
+            lines.append("  [none]")
+
         return "\n".join(lines)
 
     # Returns the value of the specified option.
@@ -319,7 +331,7 @@ class ArgSet:
     def get_args(self):
         return self.arguments
 
-    # Convenience function: attemps to parse and return the set's positional
+    # Convenience function: attempts to parse and return the set's positional
     # arguments as a list of integers.
     def get_args_as_ints(self):
         args = []
@@ -330,7 +342,7 @@ class ArgSet:
                 sys.exit("Error: cannot parse '%s' as an integer." % arg)
         return args
 
-    # Convenience function: attemps to parse and return the set's positional
+    # Convenience function: attempts to parse and return the set's positional
     # arguments as a list of floats.
     def get_args_as_floats(self):
         args = []
@@ -340,3 +352,15 @@ class ArgSet:
             except ValueError:
                 sys.exit("Error: cannot parse '%s' as a float." % arg)
         return args
+
+    # Returns true if the set contains a command.
+    def has_cmd(self):
+        return self.command is not None
+
+    # Returns the set's command string, if a command was found.
+    def get_cmd(self):
+        return self.command
+
+    # Returns the ArgSet instance for the set's command, if a command was found.
+    def get_cmd_argset(self):
+        return self.argset
