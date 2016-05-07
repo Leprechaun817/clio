15e6ef9135863fff3e61cc76a4a05867b0ae0d4c
Darren Mulholland <darren@mulholland.xyz>
2016-05-07 23:09:49 +0100

    Python: existing unit tests updated and working

--- python/test_clio.py
+++ python/test_clio.py
@@ -15,28 +15,28 @@ def test_bool_option_empty():
     parser = clio.ArgParser()
     parser.add_flag("bool")
     parser.parse([])
-    assert parser.get_opt("bool") == False
+    assert parser.get_flag("bool") == False
 
 
 def test_bool_option_missing():
     parser = clio.ArgParser()
     parser.add_flag("bool")
     parser.parse(["foo", "bar"])
-    assert parser.get_opt("bool") == False
+    assert parser.get_flag("bool") == False
 
 
 def test_bool_option_longform():
     parser = clio.ArgParser()
     parser.add_flag("bool")
     parser.parse(["--bool"])
-    assert parser.get_opt("bool") == True
+    assert parser.get_flag("bool") == True
 
 
 def test_bool_option_shortform():
     parser = clio.ArgParser()
-    parser.add_flag("bool", "b")
+    parser.add_flag("bool b")
     parser.parse(["-b"])
-    assert parser.get_opt("bool") == True
+    assert parser.get_flag("bool") == True
 
 
 def test_bool_option_dict_syntax():
@@ -53,42 +53,42 @@ def test_bool_option_dict_syntax():
 
 def test_string_option_empty():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default")
+    parser.add_str("string", "default")
     parser.parse([])
-    assert parser.get_opt("string") == "default"
+    assert parser.get_str("string") == "default"
 
 
 def test_string_option_missing():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default")
+    parser.add_str("string", "default")
     parser.parse(["foo", "bar"])
-    assert parser.get_opt("string") == "default"
+    assert parser.get_str("string") == "default"
 
 
 def test_string_option_longform():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default")
+    parser.add_str("string", "default")
     parser.parse(["--string", "value"])
-    assert parser.get_opt("string") == "value"
+    assert parser.get_str("string") == "value"
 
 
 def test_string_option_shortform():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default", "s")
+    parser.add_str("string s", "default")
     parser.parse(["-s", "value"])
-    assert parser.get_opt("string") == "value"
+    assert parser.get_str("string") == "value"
 
 
 def test_string_option_dict_syntax():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default")
+    parser.add_str("string", "default")
     parser.parse(["--string", "value"])
     assert parser["string"] == "value"
 
 
 def test_string_option_missing_value():
     parser = clio.ArgParser()
-    parser.add_str_opt("string", "default")
+    parser.add_str("string", "default")
     with pytest.raises(SystemExit):
         parser.parse(["--string"])
 
@@ -100,49 +100,49 @@ def test_string_option_missing_value():
 
 def test_int_option_empty():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101)
+    parser.add_int("int", 101)
     parser.parse([])
-    assert parser.get_opt("int") == 101
+    assert parser.get_int("int") == 101
 
 
 def test_int_option_missing():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101)
+    parser.add_int("int", 101)
     parser.parse(["foo", "bar"])
-    assert parser.get_opt("int") == 101
+    assert parser.get_int("int") == 101
 
 
 def test_int_option_longform():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101)
+    parser.add_int("int", 101)
     parser.parse(["--int", "202"])
-    assert parser.get_opt("int") == 202
+    assert parser.get_int("int") == 202
 
 
 def test_int_option_shortform():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101, "i")
+    parser.add_int("int i", 101)
     parser.parse(["-i", "202"])
-    assert parser.get_opt("int") == 202
+    assert parser.get_int("int") == 202
 
 
 def test_int_option_dict_syntax():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101)
+    parser.add_int("int", 101)
     parser.parse(["--int", "202"])
     assert parser["int"] == 202
 
 
 def test_int_option_missing_value():
     parser = clio.ArgParser()
-    parser.add_str_opt("int", 101)
+    parser.add_int("int", 101)
     with pytest.raises(SystemExit):
         parser.parse(["--int"])
 
 
 def test_int_option_invalid_value():
     parser = clio.ArgParser()
-    parser.add_int_opt("int", 101)
+    parser.add_int("int", 101)
     with pytest.raises(SystemExit):
         parser.parse(["--int", "foo"])
 
@@ -154,49 +154,49 @@ def test_int_option_invalid_value():
 
 def test_float_option_empty():
     parser = clio.ArgParser()
-    parser.add_float_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     parser.parse([])
-    assert parser.get_opt("float") == 1.1
+    assert parser.get_float("float") == 1.1
 
 
 def test_float_option_missing():
     parser = clio.ArgParser()
-    parser.add_float_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     parser.parse(["foo", "bar"])
-    assert parser.get_opt("float") == 1.1
+    assert parser.get_float("float") == 1.1
 
 
 def test_float_option_longform():
     parser = clio.ArgParser()
-    parser.add_float_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     parser.parse(["--float", "2.2"])
-    assert parser.get_opt("float") == 2.2
+    assert parser.get_float("float") == 2.2
 
 
 def test_float_option_shortform():
     parser = clio.ArgParser()
-    parser.add_float_opt("float", 1.1, "f")
+    parser.add_float("float f", 1.1)
     parser.parse(["-f", "2.2"])
-    assert parser.get_opt("float") == 2.2
+    assert parser.get_float("float") == 2.2
 
 
 def test_float_option_dict_syntax():
     parser = clio.ArgParser()
-    parser.add_float_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     parser.parse(["--float", "2.2"])
     assert parser["float"] == 2.2
 
 
 def test_float_option_missing_value():
     parser = clio.ArgParser()
-    parser.add_str_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     with pytest.raises(SystemExit):
         parser.parse(["--float"])
 
 
 def test_float_option_invalid_value():
     parser = clio.ArgParser()
-    parser.add_int_opt("float", 1.1)
+    parser.add_float("float", 1.1)
     with pytest.raises(SystemExit):
         parser.parse(["--float", "foo"])
 
@@ -209,34 +209,34 @@ def test_float_option_invalid_value():
 def test_multi_options_empty():
     parser = clio.ArgParser()
     parser.add_flag("bool1")
-    parser.add_flag("bool2", "b")
-    parser.add_str_opt("string1", "default1")
-    parser.add_str_opt("string2", "default2", "s")
-    parser.add_int_opt("int1", 101)
-    parser.add_int_opt("int2", 202, "i")
-    parser.add_float_opt("float1", 1.1)
-    parser.add_float_opt("float2", 2.2, "f")
+    parser.add_flag("bool2 b")
+    parser.add_str("string1", "default1")
+    parser.add_str("string2 s", "default2")
+    parser.add_int("int1", 101)
+    parser.add_int("int2 i", 202,)
+    parser.add_float("float1", 1.1)
+    parser.add_float("float2 f", 2.2)
     parser.parse([])
-    assert parser.get_opt("bool1") == False
-    assert parser.get_opt("bool2") == False
-    assert parser.get_opt("string1") == "default1"
-    assert parser.get_opt("string2") == "default2"
-    assert parser.get_opt("int1") == 101
-    assert parser.get_opt("int2") == 202
-    assert parser.get_opt("float1") == 1.1
-    assert parser.get_opt("float2") == 2.2
+    assert parser.get_flag("bool1") == False
+    assert parser.get_flag("bool2") == False
+    assert parser.get_str("string1") == "default1"
+    assert parser.get_str("string2") == "default2"
+    assert parser.get_int("int1") == 101
+    assert parser.get_int("int2") == 202
+    assert parser.get_float("float1") == 1.1
+    assert parser.get_float("float2") == 2.2
 
 
 def test_multi_options_longform():
     parser = clio.ArgParser()
     parser.add_flag("bool1")
-    parser.add_flag("bool2", "b")
-    parser.add_str_opt("string1", "default1")
-    parser.add_str_opt("string2", "default2", "s")
-    parser.add_int_opt("int1", 101)
-    parser.add_int_opt("int2", 202, "i")
-    parser.add_float_opt("float1", 1.1)
-    parser.add_float_opt("float2", 2.2, "f")
+    parser.add_flag("bool2 b")
+    parser.add_str("string1", "default1")
+    parser.add_str("string2 s", "default2")
+    parser.add_int("int1", 101)
+    parser.add_int("int2 i", 202,)
+    parser.add_float("float1", 1.1)
+    parser.add_float("float2 f", 2.2)
     parser.parse([
         "--bool1",
         "--bool2",
@@ -247,26 +247,26 @@ def test_multi_options_longform():
         "--float1", "3.3",
         "--float2", "4.4",
     ])
-    assert parser.get_opt("bool1") == True
-    assert parser.get_opt("bool2") == True
-    assert parser.get_opt("string1") == "value1"
-    assert parser.get_opt("string2") == "value2"
-    assert parser.get_opt("int1") == 303
-    assert parser.get_opt("int2") == 404
-    assert parser.get_opt("float1") == 3.3
-    assert parser.get_opt("float2") == 4.4
+    assert parser.get_flag("bool1") == True
+    assert parser.get_flag("bool2") == True
+    assert parser.get_str("string1") == "value1"
+    assert parser.get_str("string2") == "value2"
+    assert parser.get_int("int1") == 303
+    assert parser.get_int("int2") == 404
+    assert parser.get_float("float1") == 3.3
+    assert parser.get_float("float2") == 4.4
 
 
 def test_multi_options_shortform():
     parser = clio.ArgParser()
     parser.add_flag("bool1")
-    parser.add_flag("bool2", "b")
-    parser.add_str_opt("string1", "default1")
-    parser.add_str_opt("string2", "default2", "s")
-    parser.add_int_opt("int1", 101)
-    parser.add_int_opt("int2", 202, "i")
-    parser.add_float_opt("float1", 1.1)
-    parser.add_float_opt("float2", 2.2, "f")
+    parser.add_flag("bool2 b")
+    parser.add_str("string1", "default1")
+    parser.add_str("string2 s", "default2")
+    parser.add_int("int1", 101)
+    parser.add_int("int2 i", 202,)
+    parser.add_float("float1", 1.1)
+    parser.add_float("float2 f", 2.2)
     parser.parse([
         "--bool1",
         "-b",
@@ -277,14 +277,14 @@ def test_multi_options_shortform():
         "--float1", "3.3",
         "-f", "4.4",
     ])
-    assert parser.get_opt("bool1") == True
-    assert parser.get_opt("bool2") == True
-    assert parser.get_opt("string1") == "value1"
-    assert parser.get_opt("string2") == "value2"
-    assert parser.get_opt("int1") == 303
-    assert parser.get_opt("int2") == 404
-    assert parser.get_opt("float1") == 3.3
-    assert parser.get_opt("float2") == 4.4
+    assert parser.get_flag("bool1") == True
+    assert parser.get_flag("bool2") == True
+    assert parser.get_str("string1") == "value1"
+    assert parser.get_str("string2") == "value2"
+    assert parser.get_int("int1") == 303
+    assert parser.get_int("int2") == 404
+    assert parser.get_float("float1") == 3.3
+    assert parser.get_float("float2") == 4.4
 
 
 # --------------------------------------------------------------------------
@@ -294,15 +294,15 @@ def test_multi_options_shortform():
 
 def test_condensed_options():
     parser = clio.ArgParser()
-    parser.add_flag("bool", "b")
-    parser.add_str_opt("string", "default", "s")
-    parser.add_int_opt("int", 101, "i")
-    parser.add_float_opt("float", 1.1, "f")
+    parser.add_flag("bool b")
+    parser.add_str("string s", "default")
+    parser.add_int("int i", 101)
+    parser.add_float("float f", 1.1)
     parser.parse(["-bsif", "value", "202", "2.2"])
-    assert parser.get_opt("bool") == True
-    assert parser.get_opt("string") == "value"
-    assert parser.get_opt("int") == 202
-    assert parser.get_opt("float") == 2.2
+    assert parser["bool"] == True
+    assert parser["string"] == "value"
+    assert parser["int"] == 202
+    assert parser["float"] == 2.2
 
 
 # --------------------------------------------------------------------------
@@ -337,7 +337,7 @@ def test_positional_args():
     parser = clio.ArgParser()
     parser.parse(["foo", "bar"])
     assert parser.has_args() == True
-    assert parser.num_args() == 2
+    assert parser.len_args() == 2
     assert parser.get_arg(0) == "foo"
     assert parser.get_arg(1) == "bar"
 
@@ -371,7 +371,7 @@ def test_positional_args_as_floats():
 def test_option_parsing_switch():
     parser = clio.ArgParser()
     parser.parse(["foo", "--", "--bar", "--baz"])
-    assert parser.num_args() == 3
+    assert parser.len_args() == 3
 
 
 # --------------------------------------------------------------------------
@@ -399,9 +399,9 @@ def test_command_with_options():
     parser = clio.ArgParser()
     cmdparser = parser.add_cmd("cmd", lambda p: None, "helptext")
     cmdparser.add_flag("bool")
-    cmdparser.add_str_opt("string", "default")
-    cmdparser.add_int_opt("int", 101)
-    cmdparser.add_float_opt("float", 1.1)
+    cmdparser.add_str("string", "default")
+    cmdparser.add_int("int", 101)
+    cmdparser.add_float("float", 1.1)
     parser.parse([
         "cmd",
         "foo", "bar",
@@ -413,7 +413,7 @@ def test_command_with_options():
     assert parser.get_cmd() == "cmd"
     assert parser.get_cmd_parser() == cmdparser
     assert cmdparser.has_args() == True
-    assert cmdparser.num_args() == 2
-    assert cmdparser.get_opt("string") == "value"
-    assert cmdparser.get_opt("int") == 202
-    assert cmdparser.get_opt("float") == 2.2
+    assert cmdparser.len_args() == 2
+    assert cmdparser["string"] == "value"
+    assert cmdparser["int"] == 202
+    assert cmdparser["float"] == 2.2
