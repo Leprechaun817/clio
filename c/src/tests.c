2d03ff4a74ee8cd27e168173ead2dddbcf063290
Darren Mulholland <darren@mulholland.xyz>
2016-06-14 15:38:32 +0100

    C: add unit tests for list options

--- c/src/tests.c
+++ c/src/tests.c
@@ -56,6 +56,41 @@ void test_bool_option_shortform() {
 
 
 // -------------------------------------------------------------------------
+// Boolean lists.
+// -------------------------------------------------------------------------
+
+
+void test_bool_list_empty() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_flag_list(parser, "bool");
+    ap_parse(parser, 1, (char *[]){""});
+    assert(ap_len_list(parser, "bool") == 0);
+    ap_free(parser);
+    printf(".");
+}
+
+
+void test_bool_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_flag_list(parser, "bool");
+    ap_parse(parser, 4, (char *[]){"", "--bool", "--bool", "--bool"});
+    assert(ap_len_list(parser, "bool") == 3);
+    ap_free(parser);
+    printf(".");
+}
+
+
+void test_bool_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_flag_list(parser, "bool");
+    ap_parse(parser, 3, (char *[]){"", "-b", "-bb"});
+    assert(ap_len_list(parser, "bool") == 3);
+    ap_free(parser);
+    printf(".");
+}
+
+
+// -------------------------------------------------------------------------
 // String options.
 // -------------------------------------------------------------------------
 
@@ -92,6 +127,66 @@ void test_string_option_shortform() {
     ap_add_str(parser, "string s", "default");
     ap_parse(parser, 3, (char *[]){"", "-s", "value"});
     assert(strcmp(ap_get_str(parser, "string"), "value") == 0);
+    printf(".");
+}
+
+
+// -------------------------------------------------------------------------
+// String lists.
+// -------------------------------------------------------------------------
+
+
+void test_string_list_empty() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_str_list(parser, "str", false);
+    ap_parse(parser, 1, (char *[]){""});
+    assert(ap_len_list(parser, "str") == 0);
+    printf(".");
+}
+
+
+void test_string_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_str_list(parser, "str", false);
+    ap_parse(parser, 6, (char *[]){"", "--str", "a", "b", "--str", "c"});
+    assert(ap_len_list(parser, "str") == 2);
+    assert(strcmp(ap_get_str_list(parser, "str")[0], "a") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[1], "c") == 0);
+    printf(".");
+}
+
+
+void test_string_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_str_list(parser, "str s", false);
+    ap_parse(parser, 6, (char *[]){"", "-s", "a", "b", "-s", "c"});
+    assert(ap_len_list(parser, "str") == 2);
+    assert(strcmp(ap_get_str_list(parser, "str")[0], "a") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[1], "c") == 0);
+    printf(".");
+}
+
+
+void test_string_greedy_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_str_list(parser, "str", true);
+    ap_parse(parser, 6, (char *[]){"", "--str", "a", "b", "--str", "c"});
+    assert(ap_len_list(parser, "str") == 3);
+    assert(strcmp(ap_get_str_list(parser, "str")[0], "a") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[1], "b") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[2], "c") == 0);
+    printf(".");
+}
+
+
+void test_string_greedy_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_str_list(parser, "str s", true);
+    ap_parse(parser, 6, (char *[]){"", "-s", "a", "b", "-s", "c"});
+    assert(ap_len_list(parser, "str") == 3);
+    assert(strcmp(ap_get_str_list(parser, "str")[0], "a") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[1], "b") == 0);
+    assert(strcmp(ap_get_str_list(parser, "str")[2], "c") == 0);
     printf(".");
 }
 
@@ -138,6 +233,66 @@ void test_int_option_shortform() {
 
 
 // -------------------------------------------------------------------------
+// Integer lists.
+// -------------------------------------------------------------------------
+
+
+void test_int_list_empty() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_int_list(parser, "int", false);
+    ap_parse(parser, 1, (char *[]){""});
+    assert(ap_len_list(parser, "int") == 0);
+    printf(".");
+}
+
+
+void test_int_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_int_list(parser, "int", false);
+    ap_parse(parser, 6, (char *[]){"", "--int", "1", "2", "--int", "3"});
+    assert(ap_len_list(parser, "int") == 2);
+    assert(ap_get_int_list(parser, "int")[0] == 1);
+    assert(ap_get_int_list(parser, "int")[1] == 3);
+    printf(".");
+}
+
+
+void test_int_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_int_list(parser, "int i", false);
+    ap_parse(parser, 6, (char *[]){"", "-i", "1", "2", "-i", "3"});
+    assert(ap_len_list(parser, "int") == 2);
+    assert(ap_get_int_list(parser, "int")[0] == 1);
+    assert(ap_get_int_list(parser, "int")[1] == 3);
+    printf(".");
+}
+
+
+void test_int_greedy_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_int_list(parser, "int", true);
+    ap_parse(parser, 6, (char *[]){"", "--int", "1", "2", "--int", "3"});
+    assert(ap_len_list(parser, "int") == 3);
+    assert(ap_get_int_list(parser, "int")[0] == 1);
+    assert(ap_get_int_list(parser, "int")[1] == 2);
+    assert(ap_get_int_list(parser, "int")[2] == 3);
+    printf(".");
+}
+
+
+void test_int_greedy_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_int_list(parser, "int i", true);
+    ap_parse(parser, 6, (char *[]){"", "-i", "1", "2", "-i", "3"});
+    assert(ap_len_list(parser, "int") == 3);
+    assert(ap_get_int_list(parser, "int")[0] == 1);
+    assert(ap_get_int_list(parser, "int")[1] == 2);
+    assert(ap_get_int_list(parser, "int")[2] == 3);
+    printf(".");
+}
+
+
+// -------------------------------------------------------------------------
 // Float options.
 // -------------------------------------------------------------------------
 
@@ -174,6 +329,66 @@ void test_float_option_shortform() {
     ap_add_float(parser, "float f", 1.1);
     ap_parse(parser, 3, (char *[]){"", "-f", "2.2"});
     assert(ap_get_float(parser, "float") == 2.2);
+    printf(".");
+}
+
+
+// -------------------------------------------------------------------------
+// Float lists.
+// -------------------------------------------------------------------------
+
+
+void test_float_list_empty() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_float_list(parser, "float", false);
+    ap_parse(parser, 1, (char *[]){""});
+    assert(ap_len_list(parser, "float") == 0);
+    printf(".");
+}
+
+
+void test_float_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_float_list(parser, "flt", false);
+    ap_parse(parser, 6, (char *[]){"", "--flt", "1", "2", "--flt", "3"});
+    assert(ap_len_list(parser, "flt") == 2);
+    assert(ap_get_float_list(parser, "flt")[0] == 1.0);
+    assert(ap_get_float_list(parser, "flt")[1] == 3.0);
+    printf(".");
+}
+
+
+void test_float_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_float_list(parser, "flt f", false);
+    ap_parse(parser, 6, (char *[]){"", "-f", "1", "2", "-f", "3"});
+    assert(ap_len_list(parser, "flt") == 2);
+    assert(ap_get_float_list(parser, "flt")[0] == 1.0);
+    assert(ap_get_float_list(parser, "flt")[1] == 3.0);
+    printf(".");
+}
+
+
+void test_float_greedy_list_longform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_float_list(parser, "flt", true);
+    ap_parse(parser, 6, (char *[]){"", "--flt", "1", "2", "--flt", "3"});
+    assert(ap_len_list(parser, "flt") == 3);
+    assert(ap_get_float_list(parser, "flt")[0] == 1.0);
+    assert(ap_get_float_list(parser, "flt")[1] == 2.0);
+    assert(ap_get_float_list(parser, "flt")[2] == 3.0);
+    printf(".");
+}
+
+
+void test_float_greedy_list_shortform() {
+    ArgParser *parser = ap_new(NULL, NULL);
+    ap_add_float_list(parser, "flt f", true);
+    ap_parse(parser, 6, (char *[]){"", "-f", "1", "2", "-f", "3"});
+    assert(ap_len_list(parser, "flt") == 3);
+    assert(ap_get_float_list(parser, "flt")[0] == 1.0);
+    assert(ap_get_float_list(parser, "flt")[1] == 2.0);
+    assert(ap_get_float_list(parser, "flt")[2] == 3.0);
     printf(".");
 }
 
@@ -407,29 +622,57 @@ void test_command_with_options() {
 // -------------------------------------------------------------------------
 
 
+void line() {
+    for (int i = 0; i < 80; i++) {
+        printf("-");
+    }
+    printf("\n");
+}
+
+
 int main() {
     setbuf(stdout, NULL);
+
+    line();
     printf("Tests: ");
 
     test_bool_option_empty();
     test_bool_option_missing();
     test_bool_option_longform();
     test_bool_option_shortform();
+    test_bool_list_empty();
+    test_bool_list_longform();
+    test_bool_option_shortform();
 
     test_string_option_empty();
     test_string_option_missing();
     test_string_option_longform();
     test_string_option_shortform();
+    test_string_list_empty();
+    test_string_list_longform();
+    test_string_list_shortform();
+    test_string_greedy_list_longform();
+    test_string_greedy_list_shortform();
 
     test_int_option_empty();
     test_int_option_missing();
     test_int_option_longform();
     test_int_option_shortform();
+    test_int_list_empty();
+    test_int_list_longform();
+    test_int_list_shortform();
+    test_int_greedy_list_longform();
+    test_int_greedy_list_shortform();
 
     test_float_option_empty();
     test_float_option_missing();
     test_float_option_longform();
     test_float_option_shortform();
+    test_float_list_empty();
+    test_float_list_longform();
+    test_float_list_shortform();
+    test_float_greedy_list_longform();
+    test_float_greedy_list_shortform();
 
     test_multi_options_empty();
     test_multi_options_longform();
@@ -449,4 +692,5 @@ int main() {
     test_command_with_options();
 
     printf(" [ok]\n");
+    line();
 }
